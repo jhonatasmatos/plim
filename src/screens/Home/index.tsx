039d891ee98625177b2d6a8c06c3cb30ce50c4ae
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-10-10 19:56:10 -0300

    Initial commit 🚀

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Text } from './styles';
+
+const Home: React.FC = () => {
+
+  return (
+    <Container>
+      <Text> Hello Plim !!! </Text>
+    </Container>
+  )
+}
+
+export default Home
