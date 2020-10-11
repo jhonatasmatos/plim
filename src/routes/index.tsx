91ab492df609d47a29da7d0cca93d2bfd54d0085
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-10-11 12:46:52 -0300

    chore: added List and Detail in CreditContribution routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,13 +6,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Amount from '../screens/CreditRequest/Amount';
 import Description from '../screens/CreditRequest/Description';
 
+import List from '../screens/CreditContribution/EntrepreneursList';
+import DetailCompany from '../screens/CreditContribution/DetailCompany';
+
+
 const App = createStackNavigator()
 
 const Routes: React.FC = () => (
   <NavigationContainer>
     <App.Navigator screenOptions={{ headerShown: false }}>
-      <App.Screen name="Amount" component={Amount} />
-      <App.Screen name="Description" component={Description} />
+      {/* <App.Screen name="Amount" component={Amount} />
+      <App.Screen name="Description" component={Description} /> */}
+      <App.Screen name="List" component={List} />
+      <App.Screen name="DetailCompany" component={DetailCompany} />
     </App.Navigator>
   </NavigationContainer>
 )
