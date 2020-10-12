758c475ed997bc1d88f4eb3820a1bdf466d01f86
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-10-12 01:53:19 -0300

    chore: added Description UI

--- src/screens/CreditRequest/Description/styles.ts
+++ src/screens/CreditRequest/Description/styles.ts
@@ -3,52 +3,72 @@ import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.KeyboardAvoidingView`
   flex: 1; 
-  padding: 0 16px;
-`
+
+  background-color: #E5E5E5;
+`;
 
 export const ContainerText = styled.View`
   margin-top: 120px;
   padding: 0 35px;
-`
+`;
 
 export const Text = styled.Text`
+  font-family: 'Poppins-SemiBold';
   font-size: 18px;
   padding-top: 16px;
-`
+`;
 
 export const ContainerInput = styled.View`
   margin-top: 76px;
   padding: 0 35px;
-`
+`;
 
 export const InputText = styled.TextInput`
   font-size: 16px;
   padding: 0 10px;
   
   border-bottom-width: 1px;
-`
+`;
 
 export const ContainerButton = styled.View`
-  margin: 186px 65px;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-around;
 
-  align-items: center;
-  justify-content: center;
-`
+  position: absolute;
+  bottom: 26px;
+`;
 
-export const Button = styled(RectButton)`
-  width: 325px;
+export const ButtonGoBack = styled(RectButton)`
+  width: 152px;
   height: 54px;
-  border-radius: 10px;
+  border-radius: 14px;
 
-  align-items: center;
   justify-content: center;
+  align-items: center;
 
-  background-color: #F70F5D;
-`
+  background-color: #FFEDF3;
+`;
 
-export const TextButton = styled.Text`
+export const ButtonTextGoBack = styled.Text`
+  font-family: 'Poppins-SemiBold';
   font-size: 16px;
-  font-weight: 700;
+  color: #F70F5D;
+`;
 
+export const ButtonContinue = styled(RectButton)`
+  width: 152px;
+  height: 54px;
+  border-radius: 14px;
+
+  justify-content: center;
+  align-items: center;
+
+  background-color: #20187D;
+`;
+
+export const ButtonTextContinue = styled.Text`
+  font-family: 'Poppins-SemiBold';
+  font-size: 16px;
   color: #fff;
-`
+`;
