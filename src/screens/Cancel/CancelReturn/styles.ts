300fa5eb242a6658de07a2bfa8366092a37061a1
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-10-12 14:41:57 -0300

    chore: finished CancelReturn UI

---
+++ src/screens/Cancel/CancelReturn/styles.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+
+  background-color: #E5E5E5;  
+`;
+
+export const Text = styled.Text`
+  font-family: 'Poppins-SemiBold';
+  font-size: 18px;
+
+  padding: 36px 26px;
+`;
+
+export const ContentText = styled.Text`
+  font-family: 'Poppins-Regular';
+  font-size: 16px;
+
+  padding: 12px 26px;
+`;
+
+export const ContainerButton = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-around;
+
+  position: absolute;
+  bottom: 36px;
+`;
+
+export const ButtonGoBack = styled(RectButton)`
+  width: 152px;
+  height: 54px;
+  border-radius: 14px;
+
+  justify-content: center;
+  align-items: center;
+
+  background-color: #FFEDF3;
+`;
+
+export const ButtonTextGoBack = styled.Text`
+  font-family: 'Poppins-SemiBold';
+  font-size: 16px;
+  color: #F70F5D;
+`;
+
+export const ButtonConfirm = styled(RectButton)`
+  width: 152px;
+  height: 54px;
+  border-radius: 14px;
+
+  justify-content: center;
+  align-items: center;
+
+  background-color: #20187D;
+`;
+
+export const ButtonTextConfirm = styled.Text`
+  font-family: 'Poppins-SemiBold';
+  font-size: 16px;
+  color: #fff;
+`;
