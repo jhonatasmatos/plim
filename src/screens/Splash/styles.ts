eb3189e8f11bdd368099ef2418e4e28dbb6b956c
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-10-11 23:17:41 -0300

    chore: Finished Splash UI

---
+++ src/screens/Splash/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Text = styled.Text`
+  font-family: 'Poppins-Regular';
+  font-size: 20px;
+
+  position: absolute;
+  bottom: 36px;
+`;
+
