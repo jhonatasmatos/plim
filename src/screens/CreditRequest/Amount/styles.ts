4a3797c314d8987d88a78b4a2e164721a87f2d50
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-10-12 01:52:28 -0300

    style: Changed button in Amount UI

--- src/screens/CreditRequest/Amount/styles.ts
+++ src/screens/CreditRequest/Amount/styles.ts
@@ -4,6 +4,7 @@ import { Picker as Selected } from '@react-native-community/picker';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: #E5E5E5;
 `
 
 export const ContainerText = styled.View`
@@ -12,12 +13,13 @@ export const ContainerText = styled.View`
 `
 
 export const Text = styled.Text`
-  font-size: 18px;
+  font-family: 'Poppins-Regular';
+  font-size: 16px;
   padding-top: 16px;
 `
 
 export const ContainerInput = styled.View`
-  margin-top: 32px;
+  margin-top: 16px;
   padding: 0 35px;
 `
 
@@ -30,7 +32,7 @@ export const InputText = styled.TextInput`
 `
 
 export const ContainerPicker = styled.View`
-  margin: 32px 35px 0px;
+  margin: 5px 35px 0px;
 
   border-bottom-width: 1px;
 `
@@ -47,39 +49,64 @@ export const TextPercent = styled.Text`
 `
 
 export const ContainerAttention = styled.View`
-  margin: 32px 65px;
+  margin: 32px 42px;
+
+  padding-left: 10px;
 
   align-items: center;
   justify-content: center;
 
-  width: 280px; 
+  width: 280px;
+
+  border-left-width: 2px;
+  border-color:  #F70F5D;
 `
 
 export const TextAttention = styled.Text`
+  font-family: 'Poppins-SemiBold';
   font-size: 16px;
 `
 
 export const ContainerButton = styled.View`
-  margin: 32px 65px;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-around;
 
-  align-items: center;
-  justify-content: center;
-`
 
-export const Button = styled(RectButton)`
-  width: 325px;
+  position: absolute;
+  bottom: 26px;
+`;
+
+export const ButtonGoBack = styled(RectButton)`
+  width: 152px;
   height: 54px;
-  border-radius: 10px;
+  border-radius: 14px;
 
-  align-items: center;
   justify-content: center;
+  align-items: center;
 
-  background-color: #F70F5D;
-`
+  background-color: #FFEDF3;
+`;
 
-export const TextButton = styled.Text`
+export const ButtonTextGoBack = styled.Text`
+  font-family: 'Poppins-SemiBold';
   font-size: 16px;
-  font-weight: 700;
+  color: #F70F5D;
+`;
 
+export const ButtonContinue = styled(RectButton)`
+  width: 152px;
+  height: 54px;
+  border-radius: 14px;
+
+  justify-content: center;
+  align-items: center;
+
+  background-color: #20187D;
+`;
+
+export const ButtonTextContinue = styled.Text`
+  font-family: 'Poppins-SemiBold';
+  font-size: 16px;
   color: #fff;
-`
+`;
